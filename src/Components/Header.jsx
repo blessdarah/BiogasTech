0662b8b1648aac5f595f1c47de29df7f3aee1f54
RoslyDev <105489712+ioget@users.noreply.github.com>
2024-01-29 06:24:23 +0100

    wip: landing page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 
-const Header = () => {
-  return <h3>Header </h3>;
-};
+import { Link } from "react-router-dom";
+import { Navbar } from "./navbar";
+import { Button } from "antd";
 
-export default Header;
+
+export const Header = () => {
+  return (
+    <header style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+
+      <div>
+        <img src="/logo.png" alt="logo" width='150px' srcset="" />
+      </div>
+
+      
+
+      <nav style={{display:'inline',gap:'4px' }}>
+        <Navbar />
+      </nav>
+
+      <div style={{display:'flex',gap:'20px'}}>
+
+        <Link  to={'/auth/register'}>
+        <Button type="default">Enroll</Button>
+
+        </Link>
+
+        <Link to={'/auth/login'}>
+            <Button type="primary">Log In</Button>
+
+        </Link>
+
+      
+      </div>
+    </header>
+  );
+}
 
--- src/layout/GuestLayout.jsx
+++ src/layout/GuestLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
 import { ConfigProvider } from "antd";
-import Header from "../components/Header";
-
+import {Header} from '../Components/Header'
 export function GuestLayout() {
   return (
     <ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
+
       <Header />
+
       <Outlet />
     </ConfigProvider>
   );
