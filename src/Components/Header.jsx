fcf5e271c98ef64afe155ef508341d1d0e928e44
= <blessdarahuba@gmail.com>
2024-01-29 09:21:33 +0100

    chore: fix merge conflicts

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,5 +3,11 @@ import React from "react";
 const Header = () => {
   return <h3>Header </h3>;
 };
+<<<<<<< HEAD
 
 export default Header;
+=======
+
+export default Header;
+
+>>>>>>> bd99795 (chore: update app layouts)
